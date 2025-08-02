b392491dc877d39cdd1ae399f1694deb1172b01c
Arsh <159905020+arshc0der@users.noreply.github.com>
2025-08-02 08:11:24 +0530

    Delete assets directory

--- assets/js/nav_footer_loader.js
+++
@@ -1,3 +0,0 @@
-// Load navbar and footer
-document.getElementById('navbar').innerHTML = fetch('../components/navbar.html').then(res => res.text()).then(html => document.getElementById('navbar').innerHTML = html);
-document.getElementById('footer').innerHTML = fetch('../components/footer.html').then(res => res.text()).then(html => document.getElementById('footer').innerHTML = html);
